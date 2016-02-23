06fd1e8642a77a5ed047ef6eb3a761d26187270b
Maic Siemering <maic.siemering@volkswagen.de>
2016-02-23 21:04:42 +0100

    Add JSF

--- src/main/java/de/siemering/example/bean/HelloWorldBean.java
+++ src/main/java/de/siemering/example/bean/HelloWorldBean.java
@@ -1,13 +1,18 @@
 package de.siemering.example.bean;
 
+
 import javax.faces.bean.ManagedBean;
+import javax.faces.bean.RequestScoped;
 
 @ManagedBean
+@RequestScoped
 public class HelloWorldBean {
-   public HelloWorldBean() {
-      System.out.println("HelloWorld started!");
-   }
-   public String getMessage() {
-      return "Hello World!";
-   }
+
+    public HelloWorldBean() {
+        System.out.println("HelloWorld started!");
+    }
+
+    public String getMessage() {
+        return "Hello World!";
+    }
 }
